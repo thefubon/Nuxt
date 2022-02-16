46b84167cae8b40f63f36f10f1e45f28cf94bbaa
Promedia Style <91064803+promediastyle@users.noreply.github.com>
2022-02-16 14:32:46 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,40 @@
 <template>
-  <div class="container px-4 lg:px-8 py-10">
+  <div class="container px-4 lg:px-8 py-10 sepia:bg-gray-500">
     <h1 class="text-2xl font-bold mb-10">Welcome</h1>
 
     <Select v-model="$colorMode.preference">
         <Option v-for="item in darkModeList" :value="item.value" :key="item.value"><img class="inline-block mr-2" :src="item.icon" />{{ item.label }}</Option>
     </Select>
 
+
+    <div class="my-10">
+      <h2 class="text-2xl font-bold">Click Color Theme</h2>
+
+      <button class="button is-info is-block" @click="toggleDarkMode()">
+        <ColorScheme placeholder="..." tag="span">
+          <span v-if="$colorMode.value === 'light'">
+            <IconDark /> Dark
+          </span>
+          <span v-else>
+            <IconLight /> Light
+          </span>
+        </ColorScheme>
+      </button>
+    </div>
+
   </div>
 </template>
 
 <script>
+import IconLight from '@/assets/icons/light.svg?inline'
+import IconDark from '@/assets/icons/dark.svg?inline'
+
 export default {
   //colorMode: 'light',
+  components: {
+    IconLight,
+    IconDark
+  },
 
   data () {
     return {
@@ -40,6 +63,16 @@ export default {
 
       colors: ['system', 'light', 'dark', 'sepia'],
 
+    }
+  },
+
+  methods: {
+    toggleDarkMode() {
+      if (this.$colorMode.preference !== 'dark') {
+        this.$colorMode.preference = 'dark'
+      } else {
+        this.$colorMode.preference = 'light'
+      }
     }
   }
 

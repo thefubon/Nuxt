6c122896025968152a7b62456e2d0ae66d58b93b
Promedia Style <91064803+promediastyle@users.noreply.github.com>
2022-02-17 20:36:45 +0300

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,105 +10,31 @@ module.exports = {
     './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
+    darkSelector: '.dark-mode',
     container: {
       center: true,
     },
     extend: {
-      typography: theme => ({
-        DEFAULT: {
-          css: {
-            color: theme("colors.gray.700"),
-            figcaption: {
-              color: theme("colors.gray.900")
-            },
-            a: {
-              color: theme("colors.indigo.600"),
-              "&:hover": {
-                color: theme("colors.indigo.700")
-              },
-              "*": {
-                color: theme("colors.indigo.600")
-              },
-              code: { color: theme("colors.indigo.600") }
-            },
-            "strong > a, a > strong": {
-              color: theme("colors.indigo.600")
-            },
-            code: {
-              padding: "3px 5px",
-              borderRadius: 5,
-              background: theme("colors.gray.100")
-            },
-            mark: {
-              background: theme("colors.pink.100")
-            }
-          }
+      colors: {
+        blue: {
+          light: '#85d7ff',
+          DEFAULT: '#1fb6ff',
+          dark: '#009eeb',
         },
-        dark: {
-          css: {
-            color: theme("colors.gray.400"),
-            figcaption: {
-              color: theme("colors.gray.400")
-            },
-            a: {
-              color: theme("colors.indigo.400"),
-              "&:hover": {
-                color: theme("colors.indigo.300")
-              },
-              code: { color: theme("colors.indigo.400") },
-              "*": {
-                color: theme("colors.indigo.400"),
-                "&:hover": {
-                  color: theme("colors.indigo.300")
-                }
-              }
-            },
-            "strong > a, a > strong": {
-              color: theme("colors.indigo.400")
-            },
-            blockquote: {
-              borderLeftColor: theme("colors.gray.700"),
-              color: theme("colors.gray.300")
-            },
-            h1: {
-              color: theme("colors.white")
-            },
-            "h2,h3,h4": {
-              color: theme("colors.gray.100")
-            },
-            code: {
-              padding: "3px 5px",
-              borderRadius: 5,
-              color: theme("colors.white"),
-              background: theme("colors.gray.800")
-            },
-            "pre > code": {
-              background: "none",
-              padding: 0
-            },
-            hr: { borderColor: theme("colors.gray.700") },
-            strong: { color: theme("colors.white") },
-            thead: {
-              color: theme("colors.gray.100")
-            },
-            tbody: {
-              tr: {
-                borderBottomColor: theme("colors.gray.700")
-              }
-            },
-            mark: {
-              background: theme("colors.yellow.100")
-            }
-          }
+        pink: {
+          light: '#ff7ce5',
+          DEFAULT: '#ff49db',
+          dark: '#ff16d1',
+        },
+        gray: {
+          darkest: '#1f2d3d',
+          dark: '#3c4858',
+          DEFAULT: '#c0ccda',
+          light: '#e0e6ed',
+          lightest: '#f9fafc',
         }
-      })
+      },
     },
-    darkSelector: '.dark-mode',
-  },
-  variants: {
-    extend: {
-      typography: ["dark"]
-    }
   },
   plugins: [
     require('@tailwindcss/typography'),

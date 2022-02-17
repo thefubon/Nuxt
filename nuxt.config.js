0b59904923bd9f4691db8a081f5f658b8091396a
Promedia Style <91064803+promediastyle@users.noreply.github.com>
2022-02-17 20:05:48 +0300

    upd

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/colored-logo.svg' }
     ],
     bodyAttrs: {
       class: 'antialiased text-slate-900'
